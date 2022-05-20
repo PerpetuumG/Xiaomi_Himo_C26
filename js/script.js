ad2925135c96959bc80b3ecc041ead4382092567
PerpetuumG <perpetuumg@mail.ru>
2022-05-20 17:03:51 +0300

    add block Slider

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 2000
     })
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    })
 })
